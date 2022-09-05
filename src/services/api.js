08311971fb2b7fc272dad09b629911a8f8459c8c
Giovanna de Mello <61790986+giovannadml@users.noreply.github.com>
2022-09-05 13:46:52 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'https://omega-form-back.herokuapp.com'
-    baseURL: 'http://localhost:3001'
+    baseURL: process.env.REACT_APP_API_URL
 });
 
 export default api;
